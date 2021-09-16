e04245e60c4f67ef8ae172fcfc874b696659869b
Ronald Holshausen <rholshausen@dius.com.au>
2021-09-16 10:54:37 +1000

    chore: support JDK 8

--- provider/src/main/java/au/com/dius/pactworkshop/provider/ProductRepository.java
+++ provider/src/main/java/au/com/dius/pactworkshop/provider/ProductRepository.java
@@ -2,6 +2,8 @@ package au.com.dius.pactworkshop.provider;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -9,18 +11,22 @@ import java.util.Optional;
 @Repository
 public class ProductRepository {
 
-    private final Map<String, Product> PRODUCTS = Map.of(
-            "09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1"),
-            "10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1"),
-            "11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")
-    );
+    private final Map<String, Product> PRODUCTS = new HashMap<>();
 
     public List<Product> fetchAll() {
-        return List.copyOf(PRODUCTS.values());
+        initProducts();
+
+        return new ArrayList<>(PRODUCTS.values());
     }
 
     public Optional<Product> getById(String id) {
+        initProducts();
         return Optional.ofNullable(PRODUCTS.get(id));
     }
 
+    private void initProducts() {
+        PRODUCTS.put("09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1"));
+        PRODUCTS.put("10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1"));
+        PRODUCTS.put("11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2"));
+    }
 }
