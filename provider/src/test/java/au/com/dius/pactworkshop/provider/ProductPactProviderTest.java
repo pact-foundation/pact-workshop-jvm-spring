cae51e3a89cca1a2c0aafa405cdd02665d42d0dd
Yousaf Nabi <you@saf.dev>
2024-02-01 16:19:23 +0000

    chore: step8 update

--- provider/src/test/java/au/com/dius/pactworkshop/provider/ProductPactProviderTest.java
+++ provider/src/test/java/au/com/dius/pactworkshop/provider/ProductPactProviderTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
