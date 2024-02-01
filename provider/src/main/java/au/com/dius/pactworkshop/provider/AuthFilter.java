6813a9e736bb91b6c6cf7b20a8463bf8410b995a
Yousaf Nabi <you@saf.dev>
2024-02-01 18:16:38 +0000

    chore(deps): step9 - Java17 / Pact 4.6.5  (#33)

--- provider/src/main/java/au/com/dius/pactworkshop/provider/AuthFilter.java
+++ provider/src/main/java/au/com/dius/pactworkshop/provider/AuthFilter.java
@@ -2,13 +2,13 @@ package au.com.dius.pactworkshop.provider;
 
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
